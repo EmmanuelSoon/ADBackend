ddd0c52ab1c439cc1e163479fb6e99610ba711ff
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-07-26 10:22:27 +0800

    First Commit

---
+++ src/test/java/nus/iss/ADBackend/AdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.ADBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
