87d12d90da6d5a3bffbb977966b4d3ee73682584
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-08-01 20:00:40 +0800

    flask directory fixed

--- FlaskModel/setup.py
+++ FlaskModel/setup.py
@@ -1,13 +1,15 @@
 from functions import *
 
 def scrapImages():
+    curr_dir = os.getcwd()
     print('scrapping in progress')
-    food_list = pd.read_csv('FlaskModel/category.csv')
+    food_list = pd.read_csv(os.path.join(curr_dir,'category.csv'))
     food_list = food_list['0'].values.tolist()
     print(len(food_list))
 
+
     for curr in food_list:
-        search_and_download(curr, train_path='FlaskModel/images', test_path='FlaskModel/test_images', train_num= 20, test_num= 10)
+        search_and_download(curr, train_path= os.path.join(curr_dir,'images'), test_path=os.path.join(curr_dir,'test_images'), train_num= 20, test_num= 10)
         time.sleep(random.uniform(0.5, 1.5))    
 
 
