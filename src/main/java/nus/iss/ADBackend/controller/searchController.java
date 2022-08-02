d4ac2f36629d0ec9a523271cd40a9d96d7c653c5
alylim <lim.alywx@gmail.com>
2022-08-02 11:35:06 +0800

    added search food by calories function for android

--- src/main/java/nus/iss/ADBackend/controller/searchController.java
+++ src/main/java/nus/iss/ADBackend/controller/searchController.java
@@ -27,7 +27,18 @@ public class searchController {
     @RequestMapping("/ingredients")
     public List<Ingredient> getSearchResults(@RequestBody JSONObject response) throws IOException, ParseException{
         String search = response.getAsString("query");
-        List<Ingredient> iList = ingredientService.findSimilarIngredients(search);
+        List<Ingredient> iList;
+        try{
+            double calSearch = Double.parseDouble(search);
+            iList = ingredientService.findIngredientsWithCaloriesLowerThan(calSearch);
+            System.out.println("getting double");
+
+        }
+        catch (NumberFormatException numEx){
+            iList = ingredientService.findSimilarIngredients(search);
+            System.out.println("getting string");
+        }
+
         
         return iList;
     }
