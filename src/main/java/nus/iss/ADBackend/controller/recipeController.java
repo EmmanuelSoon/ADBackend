3e3508569876ccc11e8f0b03cbc7573e6a1f3e2d
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-08-02 12:33:17 +0800

    controller for recipes

---
+++ src/main/java/nus/iss/ADBackend/controller/recipeController.java
@@ -0,0 +1,24 @@
+package nus.iss.ADBackend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import nus.iss.ADBackend.Service.RecipeService;
+import nus.iss.ADBackend.model.*;;
+
+@RestController
+@RequestMapping(value= "/recipe", produces = "application/json")
+public class recipeController {
+    
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping("/recipe")
+    public List<Recipe> getStudents(){
+        return recipeService.getAllRecipes();
+    }
+}
