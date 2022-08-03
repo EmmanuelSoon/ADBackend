7229a9c7fed74687fa56ee5bf233e81afc98fa8d
Anandeeswaran <e0941664@u.nus.edu>
2022-08-03 23:24:50 +0800

    Login Controller changes

--- src/main/java/nus/iss/ADBackend/controller/loginController.java
+++ src/main/java/nus/iss/ADBackend/controller/loginController.java
@@ -1,7 +1,6 @@
 package nus.iss.ADBackend.controller;
 
 import java.io.IOException;
-import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,28 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.ParseException;
 import nus.iss.ADBackend.Service.UserService;
 import nus.iss.ADBackend.model.User;
 
 @RestController
-@RequestMapping(value="/login", produces ="application/json")
+@RequestMapping(value = "/login", produces = "application/json")
 public class loginController {
+
 	@Autowired
 	UserService userService;
-	
-	@RequestMapping("/authentication")
-	public String getPassword(@RequestBody JSONObject response)
-		throws IOException, ParseException{
-		
-		System.out.println("LoginController");
-		String username = response.getAsString("username");
-		System.out.println(username);
-		if(username != null) {
-			String password = userService.findUserByUsername(username).getPassword();
-			System.out.println(password);
-			return password;
-		}
-							
-		return null;
-	}	
+
+	@RequestMapping("/validateUserDetails")
+	public User validateUserDetails(@RequestBody JSONObject requestBody) throws IOException, ParseException {
+
+		User user = userService.findUserByUserNameAndPassword(requestBody.getAsString("username"),
+				requestBody.getAsString("password"));
+
+		return user;
+	}
+
 }
