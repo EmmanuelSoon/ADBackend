b84a21fffc97b33de5b5a53af00ebf9eec674dc1
Anandeeswaran <e0941664@u.nus.edu>
2022-08-06 00:18:56 +0800

    user profile details updated in register controller

--- src/main/java/nus/iss/ADBackend/controller/registerController.java
+++ src/main/java/nus/iss/ADBackend/controller/registerController.java
@@ -13,20 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 import net.minidev.json.JSONObject;
 import nus.iss.ADBackend.Service.UserService;
 import nus.iss.ADBackend.model.Goal;
+import nus.iss.ADBackend.model.Role;
 import nus.iss.ADBackend.model.User;
 
 @RestController
-@RequestMapping(value="/register", produces="application/json")
+@RequestMapping(value = "/register", produces = "application/json")
 public class registerController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@RequestMapping("/validateNewUser")
-	public User validateNewUser(@RequestBody JSONObject requestBody) throws IOException, ParseException{
+	public User validateNewUser(@RequestBody JSONObject requestBody) throws IOException, ParseException {
 		User user = userService.findUserByUsername(requestBody.getAsString("email"));
-		
-		if(user == null) {
+
+		if (user == null) {
 			User newUser = new User(requestBody.getAsString("email"), requestBody.getAsString("password"));
 			String dob = requestBody.getAsString("dob");
 			System.out.println(dob);
@@ -35,16 +36,16 @@ public class registerController {
 			System.out.println(gender);
 			newUser.setGender(gender);
 			String goal = requestBody.getAsString("goal");
-			System.out.println(goal);			
+			System.out.println(goal);
 			newUser.setGoal(getGoal(goal));
-			
+
 			userService.createUser(newUser);
 			System.out.println(newUser);
 			return newUser;
 		}
 		return null;
 	}
-	
+
 	private Goal getGoal(String goal) {
 		switch (goal) {
 		case "WEIGHTLOSS":
@@ -54,19 +55,44 @@ public class registerController {
 		case "WEIGHTMAINTAIN":
 			return Goal.WEIGHTMAINTAIN;
 		case "MUSCULE":
-			return Goal.MUSCLE;		
-		
+			return Goal.MUSCLE;
+
 		// should not come to this default
 		default:
 			return Goal.WEIGHTMAINTAIN;
 		}
 	}
+
 	private LocalDate getDate(String dob) {
 		LocalDate date;
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		date = LocalDate.parse(dob, formatter);
 		return date;
 	}
-	
+
+	@RequestMapping("/updateUserDetails")
+	public User updateUserDetails(@RequestBody JSONObject requestBody) throws IOException, ParseException {
+
+		User updatedUser = new User();
+
+		updatedUser.setId(Integer.parseInt(requestBody.getAsString("id")));
+		updatedUser.setName(requestBody.getAsString("name"));
+		updatedUser.setUsername(requestBody.getAsString("username"));
+		updatedUser.setPassword(requestBody.getAsString("password"));
+		updatedUser.setDateofbirth(LocalDate.parse(requestBody.getAsString("dateofbirth")));
+		updatedUser.setGender(requestBody.getAsString("gender"));
+		updatedUser.setGoal(Goal.valueOf(requestBody.getAsString("goal")));
+		updatedUser.setCalorieintake_limit_inkcal(
+				Double.parseDouble(requestBody.getAsString("calorieintake_limit_inkcal")));
+		updatedUser.setWaterintake_limit_inml(Double.parseDouble(requestBody.getAsString("waterintake_limit_inml")));
+		updatedUser.setRole(Role.NORMAL);
+		
+		if (userService.saveUser(updatedUser)) {
+			return userService.findUserByUserNameAndPassword(updatedUser.getUsername(), updatedUser.getPassword());
+		}
+
+		return null;
+
+	}
 
 }
