c7f5b1b0fec45aaee942daed8780650d2fc3f3eb
Anandeeswaran <e0941664@u.nus.edu>
2022-08-08 04:52:18 +0800

    insert health record when new user created

--- src/main/java/nus/iss/ADBackend/controller/registerController.java
+++ src/main/java/nus/iss/ADBackend/controller/registerController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.minidev.json.JSONObject;
+import nus.iss.ADBackend.Service.HealthRecordService;
 import nus.iss.ADBackend.Service.UserService;
 import nus.iss.ADBackend.model.Goal;
+import nus.iss.ADBackend.model.HealthRecord;
 import nus.iss.ADBackend.model.Role;
 import nus.iss.ADBackend.model.User;
 
@@ -22,6 +24,9 @@ public class registerController {
 
 	@Autowired
 	UserService userService;
+
+	@Autowired
+	HealthRecordService hrService;
 
 	@RequestMapping("/validateNewUser")
 	public User validateNewUser(@RequestBody JSONObject requestBody) throws IOException, ParseException {
@@ -42,7 +47,8 @@ public class registerController {
 
 			userService.createUser(newUser);
 			System.out.println(newUser);
-			return newUser;
+			//return newUser;
+			return userService.findUserByUserNameAndPassword(newUser.getUsername(), newUser.getPassword());
 		}
 		return null;
 	}
@@ -70,12 +76,12 @@ public class registerController {
 		date = LocalDate.parse(dob, formatter);
 		return date;
 	}
-	
+
 	private Role getRole(String role) {
-		switch(role) {
+		switch (role) {
 		case "NORMAL":
 			return Role.NORMAL;
-			
+
 		default:
 			return Role.NORMAL;
 		}
@@ -97,7 +103,7 @@ public class registerController {
 				Double.parseDouble(requestBody.getAsString("calorieintake_limit_inkcal")));
 		updatedUser.setWaterintake_limit_inml(Double.parseDouble(requestBody.getAsString("waterintake_limit_inml")));
 		updatedUser.setRole(Role.NORMAL);
-		
+
 		if (userService.saveUser(updatedUser)) {
 			return userService.findUserByUserNameAndPassword(updatedUser.getUsername(), updatedUser.getPassword());
 		}
@@ -106,4 +112,39 @@ public class registerController {
 
 	}
 
+	@RequestMapping("/insertHealthRecordonRegistration")
+	public User insertHealthRecordonRegistration(@RequestBody JSONObject requestBody)
+			throws IOException, ParseException {
+
+		User updatedUser = new User();
+
+		updatedUser.setId(Integer.parseInt(requestBody.getAsString("id")));
+		updatedUser.setName(requestBody.getAsString("name"));
+		updatedUser.setUsername(requestBody.getAsString("username"));
+		updatedUser.setPassword(requestBody.getAsString("password"));
+		updatedUser.setDateofbirth(LocalDate.parse(requestBody.getAsString("dateofbirth")));
+		updatedUser.setGender(requestBody.getAsString("gender"));
+		updatedUser.setGoal(Goal.valueOf(requestBody.getAsString("goal")));
+		updatedUser.setCalorieintake_limit_inkcal(
+				Double.parseDouble(requestBody.getAsString("calorieintake_limit_inkcal")));
+		updatedUser.setWaterintake_limit_inml(Double.parseDouble(requestBody.getAsString("waterintake_limit_inml")));
+		updatedUser.setRole(Role.NORMAL);
+
+		HealthRecord myHr = new HealthRecord();
+		myHr.setDate(LocalDate.parse(requestBody.getAsString("date")));
+		myHr.setUser(updatedUser);
+		myHr.setUserHeight(Double.parseDouble(requestBody.getAsString("user_height")));
+		myHr.setUserWeight(Double.parseDouble(requestBody.getAsString("user_weight")));
+		myHr.setCalIntake(0.0);
+		myHr.setWaterIntake(0.0);
+
+		hrService.createHealthRecord(myHr);
+
+		if (userService.saveUser(updatedUser)) {
+			return userService.findUserByUserNameAndPassword(updatedUser.getUsername(), updatedUser.getPassword());
+		}
+
+		return null;
+
+	}
 }
