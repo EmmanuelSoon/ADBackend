09602868984c18f271d8235c8f22bba92f12170d
Gaw Yan Heng <e0941670@u.nus.edu>
2022-08-14 17:15:35 +0800

    update controller to connect containers

--- src/main/java/nus/iss/ADBackend/controller/CommentController.java
+++ src/main/java/nus/iss/ADBackend/controller/CommentController.java
@@ -1,56 +1,64 @@
 package nus.iss.ADBackend.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import nus.iss.ADBackend.Service.CommentService;
-import nus.iss.ADBackend.Service.IngredientService;
 import nus.iss.ADBackend.Service.RecipeService;
 import nus.iss.ADBackend.Service.UserService;
 import nus.iss.ADBackend.helper.CommentForm;
 import nus.iss.ADBackend.model.Comment;
 import nus.iss.ADBackend.model.Recipe;
 import nus.iss.ADBackend.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
-@RequestMapping(value= "/comment", produces = "application/json")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(value = "/comment", produces = "application/json")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CommentController {
-    @Autowired
-    private RecipeService recipeService;
-    @Autowired
-    private CommentService commentService;
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private RecipeService recipeService;
+	@Autowired
+	private CommentService commentService;
+	@Autowired
+	private UserService userService;
 
-    @GetMapping("/recipe/{id}")
-    public List<Comment> getCommentsByRecipeId(@PathVariable int id) {
-        return commentService.findCommentsByRecipeId(id);
-    }
-    @PostMapping ("/create")
-    public ResponseEntity createComment(@RequestBody CommentForm commentForm) {
-        User u = userService.findUserById(commentForm.getUserId());
-        Recipe recipe = recipeService.findRecipeById(commentForm.getRecipeId());
-        if (u == null || recipe == null) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        String content = commentForm.getContent();
-        double rating = commentForm.getRating();
-        Comment comment = new Comment(rating, content, u, LocalDateTime.now(), recipe);
-        commentService.createComment(comment);
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
+	@GetMapping("/recipe/{id}")
+	public List<Comment> getCommentsByRecipeId(@PathVariable int id) {
+		return commentService.findCommentsByRecipeId(id);
+	}
 
-    @GetMapping("/user/{id}")
-    public List<Comment> getCommentsByUserId(@PathVariable int id) {
-        return commentService.findCommentsByUserId(id);
-    }
-    @GetMapping("/delete/{id}")
-    public void deleteCommentsById(@PathVariable int id) {
-        commentService.deleteCommentById(id);
-    }
+	@PostMapping("/create")
+	public ResponseEntity createComment(@RequestBody CommentForm commentForm) {
+		User u = userService.findUserById(commentForm.getUserId());
+		Recipe recipe = recipeService.findRecipeById(commentForm.getRecipeId());
+		if (u == null || recipe == null) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		String content = commentForm.getContent();
+		double rating = commentForm.getRating();
+		Comment comment = new Comment(rating, content, u, LocalDateTime.now(), recipe);
+		commentService.createComment(comment);
+		return new ResponseEntity<>(null, HttpStatus.OK);
+	}
+
+	@GetMapping("/user/{id}")
+	public List<Comment> getCommentsByUserId(@PathVariable int id) {
+		return commentService.findCommentsByUserId(id);
+	}
+
+	@GetMapping("/delete/{id}")
+	public void deleteCommentsById(@PathVariable int id) {
+		commentService.deleteCommentById(id);
+	}
 }
--- src/main/java/nus/iss/ADBackend/controller/ReportController.java
+++ src/main/java/nus/iss/ADBackend/controller/ReportController.java
@@ -1,43 +1,48 @@
 package nus.iss.ADBackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import nus.iss.ADBackend.Service.CommentService;
 import nus.iss.ADBackend.Service.RecipeService;
 import nus.iss.ADBackend.Service.ReportService;
 import nus.iss.ADBackend.Service.UserService;
-import nus.iss.ADBackend.helper.CommentForm;
 import nus.iss.ADBackend.helper.ReportForm;
-import nus.iss.ADBackend.model.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
+import nus.iss.ADBackend.model.Recipe;
+import nus.iss.ADBackend.model.Report;
+import nus.iss.ADBackend.model.ReportCategory;
+import nus.iss.ADBackend.model.User;
 
 @RestController
-@RequestMapping(value= "/report", produces = "application/json")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(value = "/report", produces = "application/json")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ReportController {
-    @Autowired
-    private RecipeService recipeService;
-    @Autowired
-    private CommentService commentService;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private ReportService reportService;
+	@Autowired
+	private RecipeService recipeService;
+	@Autowired
+	private CommentService commentService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private ReportService reportService;
 
-    @PostMapping("/create")
-    public ResponseEntity createComment(@RequestBody ReportForm reportForm) {
-        User u = userService.findUserById(reportForm.getUserId());
-        Recipe recipe = recipeService.findRecipeById(reportForm.getRecipeId());
-        String content = reportForm.getContent();
-        ReportCategory reportCategory = ReportCategory.values()[reportForm.getCategoryId()];
-        if (u == null || recipe == null) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-        Report report = new Report(content, u, recipe, reportCategory);
-        reportService.createReport(report);
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
+	@PostMapping("/create")
+	public ResponseEntity createComment(@RequestBody ReportForm reportForm) {
+		User u = userService.findUserById(reportForm.getUserId());
+		Recipe recipe = recipeService.findRecipeById(reportForm.getRecipeId());
+		String content = reportForm.getContent();
+		ReportCategory reportCategory = ReportCategory.values()[reportForm.getCategoryId()];
+		if (u == null || recipe == null) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		Report report = new Report(content, u, recipe, reportCategory);
+		reportService.createReport(report);
+		return new ResponseEntity<>(null, HttpStatus.OK);
+	}
 }
